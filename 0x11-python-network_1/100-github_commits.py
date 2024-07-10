f51079e1bfe0bc10fbdd2233e54cc2d828487400
Ohimai Matthew <matthew4ohimai15@gmail.com>
2024-07-09 18:21:21 -0700

    Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists 10 commits of a repository by a specific user using the GitHub API"""
+import requests
+import sys
+
+if __name__ == "__main__":
+	repo = sys.argv[1]
+	owner = sys.argv[2]
+	url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+	response = requests.get(url)
+	commits = response.json()
+
+	for commit in commits[:10]:
+		sha = commit.get('sha')
+		author = commit.get('commit').get('author').get('name')
+		print(f"{sha}: {author}")
